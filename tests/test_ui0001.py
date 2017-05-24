8672117ea535e6e67e23f6f5f883baf73c40709c
jintaka1989 <jintaka198912@gmail.com>
2017-05-24 17:26:41 +0900

    push using upload.py

---
+++ tests/test_ui0001.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+# import pdb; pdb.set_trace()
+import unittest
+import os
+from selenium import webdriver
+
+test_id = 'uitest0001'
+tmp = 'tmp/'
+DIRNAME = os.path.dirname(os.path.abspath(__file__))
+if os.path.isdir(DIRNAME+'/'+tmp) == False:
+    os.mkdir(DIRNAME+'/'+tmp)
+
+FILENAME = os.path.join(os.path.dirname(os.path.abspath(__file__)), tmp+test_id + ".png")
+
+class uitest0001(unittest.TestCase):
+    def setUp(self):
+        self.browser = webdriver.Chrome()
+        
+    def test_uitest0001(self):
+        browser = self.browser
+        browser.set_window_size(1280, 1280)
+        browser.get('http://localhost:8000/admin')
+        browser.find_element_by_id("id_username").send_keys("jintaka")
+        browser.find_element_by_id("id_password").send_keys("pa$$word")
+        browser.find_element_by_class_name("submit-row").click()
+        home_link = browser.find_element_by_link_text('サイトを表示')
+        home_link.click()
+        browser.save_screenshot(FILENAME)
+        expected = 'Django Tutorial blog'
+
+        print("==TestResult=========================")
+        if browser.title == expected:
+            print("OK!")
+            print('Expected:'+expected)
+            print('Result  :'+browser.title)
+        else:
+            print("NG")
+            print('Expected:'+expected)
+            print('Result     :'+browser.title)
+        print("=====================================")
+    
+    def tearDown(self):
+        self.browser.quit()
+
+# unittest.main()
+
+if __name__ == "__main__":
+    unittest.main()
